129fc60cc9cc113c449d78b77b8295874f03d0d2
Philip Helger <philip@helger.com>
2026-07-20 22:58:12 +0200

    Some cleansing

--- phoss-ap-extension-demo/run-docker-ph.sh
+++ phoss-ap-extension-demo/run-docker-ph.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Example script that runs the Docker image built in this submodule via "assemble.sh".
+# It selects the same image name assemble.sh produces: the "-arm64" variant on Mac
+# (Apple Silicon), the plain variant elsewhere. Override by setting IMAGE_NAME.
+if [ "$(uname -s)" = "Darwin" ]; then
+  IMAGE_NAME="${IMAGE_NAME:-phelger/phoss-ap-with-demo-ext-arm64}"
+else
+  IMAGE_NAME="${IMAGE_NAME:-phelger/phoss-ap-with-demo-ext}"
+fi
+
 docker stop phoss-ap-with-demo-ext
 docker rm phoss-ap-with-demo-ext
 docker run -d \
@@ -17,4 +26,4 @@ docker run -d \
   -e ORG_APACHE_WSS4J_CRYPTO_MERLIN_KEYSTORE_PRIVATE_PASSWORD=peppol \
   -v ./generated/data:/var/phoss-ap/data \
   -v /Users/philip/dev/git/phoss-ap/phoss-ap-webapp/src/main/resources/test-ap-2025-g3.p12:/config/keystore.p12 \
-  phelger/phoss-ap-with-demo-ext-arm64
+  "${IMAGE_NAME}"
